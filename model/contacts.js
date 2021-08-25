007f0db2204039f5435cfbbfb0233132bb4ffb73
mitrofin <43621297+mitrofin@users.noreply.github.com>
2021-08-25 21:48:58 +0300

    Update contacts.js

--- model/contacts.js
+++ model/contacts.js
@@ -49,45 +49,6 @@ const updateContact = async (contactId, body) => {
   return newContact.id ? newContact : null;
 };
 
-/* function listContacts() {
-  fs.readFile(contactsPath)
-    .then((data) => console.table(JSON.parse(data)))
-    .catch((error) => console.log(error.message));
-}
-
-function getContactById(contactId) {
-  fs.readFile(contactsPath)
-    .then((data) => {
-      const users = JSON.parse(data);
-      const user = users.filter((user) => user.id == contactId);
-      console.table(user);
-    })
-    .catch((error) => console.log(error.message));
-}
-
-function removeContact(contactId) {
-  fs.readFile(contactsPath)
-    .then((data) => {
-      const users = JSON.parse(data);
-      const usersFiltered = users.filter((user) => user.id != contactId);
-      fs.writeFile(contactsPath, JSON.stringify(usersFiltered, null, "\t"));
-      console.log("Contact removed");
-    })
-    .catch((error) => console.log(error.message));
-}
-
-function addContact(name, email, phone) {
-  fs.readFile(contactsPath)
-    .then((data) => {
-      const users = JSON.parse(data);
-      const id = Date.now();
-      users.push({ id: id, name: name, email: email, phone: phone });
-      fs.writeFile(contactsPath, JSON.stringify(users, null, "\t"));
-      console.log("Contact added");
-    })
-    .catch((error) => console.log(error.message));
-}  */
-
 module.exports = {
   listContacts,
   getContactById,
